953a41c1df1e243cde74330250e11e14f2cf47f6
Dan McGuinness <danm@versal.com>
2014-08-22 12:08:20 -0700

    Get rid of models

--- scripts/components/app_component.js
+++ scripts/components/app_component.js
@@ -1,34 +1,42 @@
 /** @jsx React.DOM */
 var React = require('react');
-var AppModel = require('../models/app_model'),
-    LearnerModel = require('../models/learner_model');
 
 module.exports = React.createClass({
+
+  getInitialState: function() {
+    return {editable: false};
+  },
   save: function (attributes) {
-    if (!this.props.model) {
+    if (!this.props.attributes) {
       return;
     }
-    this.props.model.set(attributes);
+    this.props.player.setAttributes(attributes);
+  },
+
+  onAttributesChanged: function (attributes) {
+    this.setState({attributes: attributes});
+  },
+
+  onLearnerStateChanged: function (learnerState) {
+    this.setState({learnerState: learnerState});
+  },
+
+  onEditableChanged: function (data) {
+    this.setState({editable: data.editable});
   },
 
   componentDidMount: function() {
-    var player = this.props.player;
-    this.appModel = new AppModel({}, {
-      player: player,
-      component: this
-    });
-
-    this.learnerModel = new LearnerModel({}, {
-      player: player,
-      component: this
-    });
-
     player.startListening();
     player.watchBodyHeight();
   },
 
+  componentWillMount: function() {
+    this.props.player.on('attributesChanged', this.onAttributesChanged.bind(this))
+    this.props.player.on('learnerStateChanged', this.onLearnerStateChanged.bind(this))
+    this.props.player.on('editableChanged', this.onEditableChanged.bind(this));
+  },
+
   render: function() {
-    console.log(this.state);
-    return <div>{this.props.editable ? "author" : "learn"}</div>;
+    return <div>{this.state.editable ? "author" : "learn"}</div>;
   }
 })
--- scripts/gadget.js
+++ scripts/gadget.js
@@ -6,22 +6,6 @@ var AppComponent = require('./components/app_component');
 function Gadget(options) {
   this.el = options.el;
   this.player = options.player;
-
-  this.editable = false;
-  this.initPlayerInteraction(options.player);
-  this.render();
-};
-
-Gadget.prototype.initPlayerInteraction = function (player) {
-  player.on('editableChanged', this.onEditableChanged.bind(this));
-};
-
-Gadget.prototype.onEditableChanged = function (data) {
-  this.editable = data.editable;
-  this.render();
-};
-
-Gadget.prototype.render = function () {
   React.renderComponent(<AppComponent player={this.player} editable={this.editable} />, this.el);
 };
 
