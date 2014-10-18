f695c6e0bed3e0c9cb3f6173759b9ca3b47def50
Shaohua Zhou <shao@versal.com>
2014-10-18 14:57:17 -0400

    Cleanup

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ react-boilerplate-gadget
 ========================
 
 
+### how to develop
+```
+npm install -g bower
+npm install -g stylus
+npm install -g gulp
+npm install
+bower install
+gulp dev
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp       = require('gulp'),
     react      = require('gulp-react'),
     concat     = require('gulp-concat'),
+    stylus     = require('gulp-stylus'),
+    nib        = require('nib'),
     browserify = require('gulp-browserify');
 
 
@@ -13,13 +15,19 @@ gulp.task('browserify', function() {
     .pipe(gulp.dest('./'));
 });
 
+gulp.task('styl', function() {
+  gulp.src('./css/gadget.styl')
+    .pipe(stylus({use: [nib()]}))
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest('./'));
+});
+
 gulp.task('default', function() {
-  gulp.run('browserify');
+  gulp.run('browserify', 'styl');
 });
 
 gulp.task('dev', function() {
   gulp.run('default');
-
   gulp.watch( './scripts/**/*.js', [ 'browserify']);
   gulp.watch('**/*.styl', ['styl']);
 });
